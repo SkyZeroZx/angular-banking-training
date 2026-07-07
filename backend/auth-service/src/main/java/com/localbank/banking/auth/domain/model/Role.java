91c698d98329c10c251b95ae4b0632c47f3bdc39
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2026-07-06 21:16:11 -0500

    Backport backend to NestJS monolith

--- backend/auth-service/src/main/java/com/localbank/banking/auth/domain/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.localbank.banking.auth.domain.model;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- backend/auth-service/src/main/java/com/localbank/banking/auth/domain/port/PasswordEncoderPort.java
+++
@@ -1,6 +0,0 @@
-package com.localbank.banking.auth.domain.port;
-
-public interface PasswordEncoderPort {
-
-    String encode(String rawPassword);
-}
